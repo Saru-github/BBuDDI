bb7d0ef81ab173c9e383349c50ef1e9578a17a18
Saru <heo_dar@naver.com>
2022-04-06 16:05:37 +0900

    first commit

---
+++ src/main/java/com/buddi/BuddiApplication.java
@@ -0,0 +1,13 @@
+package com.buddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddiApplication.class, args);
+	}
+
+}
